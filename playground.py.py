0a9ba8700833606022460f747bc298a03318233f
PDHS lab <pdhslab@DSUSD.K12.CA.US>
2017-01-05 10:47:22 -0800

    1/5/17

--- playground.py.py
+++ playground.py.py
@@ -1,16 +1,45 @@
 from Tkinter import * #gives us access to everything in the Tkinter class
+import tkMessageBox
+
+def buttonpress():
+    entrytxt = entry1.get()
+    print entrytxt
+    tkMessageBox.showinfo("Here's what you typed", entrytxt)
+    
+def addtolist():
+    entrytxt = entry1.get()
+    listbox1.insert(END, entrytxt)
+    
+    
+def addtolist2(event):
+    entrytxt = entry1.get()
+    listbox1.insert(END, entrytxt)
+    entry1.delete(0, END)
+        
+def clearlist(event):
+    listbox1.delete(0, END)
+
 
 root = Tk() #gives us a blank canvas object to work with
 root.title("Gooey 'n Tink")
 
-button1 = Button(root, text="Button 1")
+button1 = Button(root, text="Button 1", command=addtolist)
 button1.grid(row=1, column=1)
 
 entry1 = Entry(root)
 entry1.grid(row=1, column=0)
+entry1.bind("<Return>", addtolist2)
 
 label1 = Label(root, text="Hello World", bg="pink", anchor=W)
-label1.grid(row=0, column=0, sticky=EW)
+label1.grid(row=0, column=0, sticky=EW, columnspan=2)
 
+listbox1 = Listbox(root)
+listbox1.grid(row=2, column=0, sticky=EW, columnspan=2)
+listbox1.bind("<Button-3>", clearlist)
+
+listbox1.insert(END, "Bob")
+listbox1.insert(END, "Joffrey")
+listbox1.insert(END, "Severus Snape")
+listbox1.insert(END, "Dudley")
 
 mainloop() #causes the windows to display on the screen until program closes
